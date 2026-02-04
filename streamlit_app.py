ef8eca476a449118e2fcbed59a26a6d5bc1430f7
DrDreMax <158600087+DrDreMax@users.noreply.github.com>
2026-02-04 16:15:15 -0500

    Remove version from Smoothie title
    Version title was removed

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize your Smoothie :cup_with_straw: {st.__version__}")
+st.title(f":cup_with_straw: Customize your Smoothie :cup_with_straw: ")
 st.write(
   """Choose the fruits you want in your custom Smoothie!  
   """
