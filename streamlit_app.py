032caaf643e58cb6a69e2aacc69a528c9f4eee8d
DrDreMax <158600087+DrDreMax@users.noreply.github.com>
2026-02-11 23:35:28 -0500

    Fix column name reference in dataframe filtering
    Updated column name reference from 'FRUIT_NAME' to 'fruit_name' for consistency.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col ('SEA
 #Convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
-st.write(pd_df.columns)
-st.stop()
+#st.write(pd_df.columns)
+#st.stop()
+
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients: '
@@ -48,7 +49,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + 'Nutrition Information')
