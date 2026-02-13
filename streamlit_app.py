2fa4f874880b6708a07339a7564b8c5e94a83290
DrDreMax <158600087+DrDreMax@users.noreply.github.com>
2026-02-12 20:25:25 -0500

    Fix string quotes in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ st.write(
 )
 
 
-name_on_order = st.text_input("Name on Smoothie")
-st.write("The name on your Smoothie will be: ", name_on_order)
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be:', name_on_order)
 
 
 
